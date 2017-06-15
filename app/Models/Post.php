d6adecdffccae8091ac1069b268c25e3c3896035
vietpnk53 <vietpnk53@gmail.com>
2017-06-15 09:54:54 +0700

    update

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,8 +6,24 @@ use Eloquent as Model;
 
 /**
  * Class Post
+ *
  * @package App\Models
  * @version June 15, 2017, 2:07 am UTC
+ * @property int $id
+ * @property string $title
+ * @property string $body
+ * @property string $slug
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property string $deleted_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Post whereBody($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Post whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Post whereDeletedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Post whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Post whereSlug($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Post whereTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Post whereUpdatedAt($value)
+ * @mixin \Eloquent
  */
 class Post extends Model
 {
